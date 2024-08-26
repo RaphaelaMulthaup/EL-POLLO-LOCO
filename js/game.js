afac47574892ea2aa90a588eb8d74750cb416b74
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2024-08-26 09:51:06 +0200

    object keyboard

--- js/game.js
+++ js/game.js
@@ -1,9 +1,15 @@
 let canvas;
 let world;
+let keyboard = new Keyboard();
 
 function init(){
     canvas = document.getElementById('canvas');
     world = new World(canvas);
     console.log('My character is ', world.character);
-    
 }
+
+
+window.addEventListener('keypress', (event) => {
+    console.log(event);
+});
+
