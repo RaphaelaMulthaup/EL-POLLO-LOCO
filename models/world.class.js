fea015faa7e723ab1c9bedc3b1c0518320c09d06
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2024-08-29 09:37:26 +0200

    move cammera

--- models/world.class.js
+++ models/world.class.js
@@ -17,6 +17,7 @@ class World {
     canvas;
     ctx;
     keyboard;
+    camera_x = 0;
 
     constructor(canvas, keyboard){
         this.ctx = canvas.getContext('2d');
@@ -32,6 +33,8 @@ class World {
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        
+        this.ctx.translate(this.camera_x, 0);
 
         this.addObjectsToMap(this.backgroundObjects);
         this.addObjectsToMap(this.clouds);
@@ -39,6 +42,8 @@ class World {
         this.addToMap(this.character);
 
         this.addObjectsToMap(this.enemies);
+
+        this.ctx.translate(-this.camera_x, 0);
 
         // draw() wird immer wieder aufgerufen
         let self = this;
