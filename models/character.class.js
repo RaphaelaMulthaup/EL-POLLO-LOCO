76c4f5675d2adb52eda4eb510df3087ca182e4d1
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2024-09-12 09:23:24 +0200

    jump animation

--- models/character.class.js
+++ models/character.class.js
@@ -12,6 +12,17 @@ class Character extends MovableObject {
         '../img/2_character_pepe/2_walk/W-25.png',
         '../img/2_character_pepe/2_walk/W-26.png'
     ];
+    IMAGES_JUMPING = [
+        '../img/2_character_pepe/3_jump/J-31.png',
+        '../img/2_character_pepe/3_jump/J-32.png',
+        '../img/2_character_pepe/3_jump/J-33.png',
+        '../img/2_character_pepe/3_jump/J-34.png',
+        '../img/2_character_pepe/3_jump/J-35.png',
+        '../img/2_character_pepe/3_jump/J-36.png',
+        '../img/2_character_pepe/3_jump/J-37.png',
+        '../img/2_character_pepe/3_jump/J-38.png',
+        '../img/2_character_pepe/3_jump/J-39.png'
+    ];
     world;
     speed = 5;
     otherDirection = false;
@@ -20,6 +31,7 @@ class Character extends MovableObject {
     constructor(){
         super().loadImg('../img/2_character_pepe/2_walk/W-21.png');
         this.loadImages(this.IMAGES_WALKING);
+        this.loadImages(this.IMAGES_JUMPING);
         this.applyGravity();
         this.walking_sound.volume = 0.4;
         this.animate();
@@ -42,10 +54,14 @@ class Character extends MovableObject {
         }, 1000 / 60);  
 
         setInterval(() => {
-            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
-                // Walk animation
-                this.playAnimation(this.IMAGES_WALKING);
-            }    
+            if (this.isAboveGround()) {
+                this.playAnimation(this.IMAGES_JUMPING);
+            } else {
+                if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                    // Walk animation
+                    this.playAnimation(this.IMAGES_WALKING);
+                } 
+            }
         }, 100);  
     }
 
