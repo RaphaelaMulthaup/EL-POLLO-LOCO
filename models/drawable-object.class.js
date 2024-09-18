29306cb55eacab462a0cdc3d3bc9f6627d5caeb6
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2024-09-18 08:42:36 +0200

    new classes DrawableObject and StatusBar

---
+++ models/drawable-object.class.js
@@ -0,0 +1,28 @@
+class DrawableObject {
+    x;
+    y;
+    img;
+    height;
+    width;
+    imageCache = {};
+    currentImage = 0;
+
+    loadImg(path) {
+        this.img = new Image();
+        this.img.src = path;
+    }
+
+    loadImages(arr){
+        arr.forEach(path => {
+            let img = new Image();
+            img.src = path;
+            this.imageCache[path] = img;
+        });
+
+    }
+
+    draw(ctx){
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+}
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,11 +1,4 @@
-class MovableObject {
-    x;
-    y;
-    img;
-    height;
-    width;
-    imageCache = {};
-    currentImage = 0;
+class MovableObject extends DrawableObject{
     speed;
     speedY = 0;
     acceleration = 3;
@@ -23,24 +16,6 @@ class MovableObject {
 
     isAboveGround(){
         return this.y < 145;
-    }
-
-    loadImg(path) {
-        this.img = new Image();
-        this.img.src = path;
-    }
-
-    loadImages(arr){
-        arr.forEach(path => {
-            let img = new Image();
-            img.src = path;
-            this.imageCache[path] = img;
-        });
-
-    }
-
-    draw(ctx){
-        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
     drawFrame(ctx){
