fe05313aa67792b0de8bb4f8c0be02b98e68f6b0
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2024-09-26 08:38:50 +0200

    sound coin animation

--- models/coin-animation.class.js
+++ models/coin-animation.class.js
@@ -1,4 +1,7 @@
 class CoinAnimation extends MovableObject {
+
+sound = new Audio('../audio/coin.mp3');
+
     constructor(){
         super();
         this.loadImg('../img/8_coin/coin_1.png');
@@ -7,6 +10,7 @@ class CoinAnimation extends MovableObject {
         this.width = 100;
         this.height = 100;
         this.moveUpwarts();
+        this.sound.play();
     }
 
     moveUpwarts(){
