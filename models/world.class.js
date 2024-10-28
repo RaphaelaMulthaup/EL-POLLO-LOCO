0fba7d2246f755f0a3cb25d84f3f5cc7bd926947
RaphaelaMulthaup <119539243+RaphaelaMulthaup@users.noreply.github.com>
2024-10-28 11:26:13 +0100

    background color

--- models/world.class.js
+++ models/world.class.js
@@ -143,12 +143,16 @@ class World {
     }
 
     collectBottle(bottle, indexOfObj){
-        bottle.collect_bottle_sound.volume = 0.4;
-        bottle.collect_bottle_sound.currentTime = 0.6;
-        bottle.collect_bottle_sound.play();
+        this.playCollectBottleSound(bottle);
         this.level.collectibleObjects.splice(indexOfObj, 1);
         this.collectedBottles += 1;
         this.statusBarBottles.setPercentage(this.collectedBottles * 20);
+    }
+
+    playCollectBottleSound(bottle){
+        bottle.collect_bottle_sound.volume = 0.4;
+        bottle.collect_bottle_sound.currentTime = 0.6;
+        bottle.collect_bottle_sound.play();
     }
 
     collectCoin(coin, indexOfObj){
@@ -195,13 +199,17 @@ class World {
             if (this.keyboard.D && currentTime - this.lastThrowTime >= 500 && this.collectedBottles != 0  && !this.introAnimationEndboss && !this.character.isHurt() && !this.character.isDead()) {
                 let throwableBottle = new ThrowableBottle(this.character);
                 this.throwableBottles.push(throwableBottle);
-                throwableBottle.throwing_bottle_sound.currentTime = 0.053;
-                throwableBottle.throwing_bottle_sound.play();
+                this.playThrowingBottleSound(throwableBottle);
                 this.lastThrowTime = currentTime;
                 this.collectedBottles -= 1;
                 this.statusBarBottles.setPercentage(this.collectedBottles * 20);
             }
         }, 1000 / 60);
+    }
+
+    playThrowingBottleSound(throwableBottle){
+        throwableBottle.throwing_bottle_sound.currentTime = 0.053;
+        throwableBottle.throwing_bottle_sound.play();
     }
 
     checkCollisionsBottlesEmemies(){
